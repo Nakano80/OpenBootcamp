6ca93d79f93b8c0c54676efd3fb629cd67db3d1c
Nakano80 <alexperalta1208@gmail.com>
2023-01-31 23:31:38 -0500

    Class Coche

--- Intro_Progra/Tema_3.java
+++ Intro_Progra/Tema_3.java
@@ -1,21 +1,25 @@
 public class Tema_3 {
     public static void main(String[] args) {
-        int resultado=0;
-        resultado=suma(4,15,6);
+        suma(4,15,6);
 
-        Coche Toyota = new Coche();
-        System.out.println(Toyota.SumaPuertas());
+        Coche miCoche = new Coche();
+        miCoche.sumarPuertas();
+        miCoche.sumarPuertas();
+
+        System.out.println(miCoche.numeroDePuertas);
     }
 
-    public static int suma(int a,int b,int c){
-        return a+b+c;
+    public static void suma(int a,int b,int c){
+        int resultado;
+        resultado = a+b+c;
+        System.out.println(resultado);
     }
 
 }
 class Coche{
-    public int puertas=2;
+    public int numeroDePuertas=4;
 
-    public static void SumaPuertas(){
-        this.puertas++;
+    public void sumarPuertas(){
+        this.numeroDePuertas++;
     }
 }
