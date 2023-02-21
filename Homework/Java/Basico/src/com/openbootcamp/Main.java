1e3dda99f79e20f1170aa50757e298e4379fa8f9
Nakano80 <alexperalta1208@gmail.com>
2023-02-20 23:07:32 -0500

    Tema 1

---
+++ Homework/Java/Basico/src/com/openbootcamp/Main.java
@@ -0,0 +1,35 @@
+package com.openbootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+        //1. Numericos
+        //1.1 Enteros.
+        byte variable1=4;
+        short variable2=10;
+        int variable3=30;
+        long variable4=100;
+
+        //1.2 Decimales
+        float variable5=5.5f;
+        double variable6=10.5d;
+
+        //2. Booleano
+        boolean variable7=true;
+        boolean variable8=false;
+
+        //3. Texto
+        char variable9='a';
+        String variable10="Lorem ipsum";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println(variable9);
+        System.out.println(variable10);
+    }
+}
