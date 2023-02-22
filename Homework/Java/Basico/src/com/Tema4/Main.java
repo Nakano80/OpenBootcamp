ef780468b6cab6d7c9427c48c97f4469f0602d79
Nakano80 <alexperalta1208@gmail.com>
2023-02-22 17:27:08 -0500

    Tema 4

---
+++ Homework/Java/Basico/src/com/Tema4/Main.java
@@ -0,0 +1,15 @@
+package com.Tema4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SmartDevice smartDevice=new SmartDevice();
+        SmartPhone smartPhone=new SmartPhone("Redmi","Note 11","Android",true,600);
+        Smartwatch smartwatch=new Smartwatch("Samsung","Galaxy","Android",false,350,"Blanco");
+
+        System.out.println("Celular: "+smartPhone.marca +" "+ smartPhone.modelo+"/ Precio: $"+smartPhone.precio +" Dólares");
+        System.out.println("SmartWatch: "+smartwatch.marca +" "+ smartwatch.modelo+"/ Precio: $"+smartwatch.precio +" Dólares y tiene un colo: "+smartwatch.color);
+
+
+    }
+}
---
+++ Homework/Java/Basico/src/com/Tema4/SmartDevice.java
@@ -0,0 +1,37 @@
+package com.Tema4;
+
+public class SmartDevice {
+    String marca;
+    String modelo;
+    String sistOp;
+    Boolean linterna;
+
+    public SmartDevice(){
+
+    }
+
+    public SmartDevice(String marca,String modelo, String sistOp, Boolean linterna) {
+        this.marca = marca;
+        this.modelo=modelo;
+        this.sistOp = sistOp;
+        this.linterna = linterna;
+    }
+}
+
+class SmartPhone extends SmartDevice{
+    int precio;
+
+    public SmartPhone(String marca, String modelo, String sistOp, Boolean linterna, int precio) {
+        super(marca, modelo, sistOp, linterna);
+        this.precio = precio;
+    }
+}
+
+class Smartwatch extends SmartPhone{
+    String color;
+
+    public Smartwatch(String marca, String modelo, String sistOp, Boolean linterna, int precio, String color) {
+        super(marca, modelo, sistOp, linterna, precio);
+        this.color=color;
+    }
+}
